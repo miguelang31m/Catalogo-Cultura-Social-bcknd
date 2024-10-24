da6d70c81ec2a572a10b13f24d25b6e085cd7ea5
Miguel <miguelangelsbf@gmail.com>
2024-10-23 23:01:27 -0500

    Implemented business logic for finding videos by title and duration in VideoService, along with corresponding test cases in VideoServiceTest.

--- src/test/java/VideoServiceTest.java
+++ src/test/java/VideoServiceTest.java
@@ -37,4 +37,52 @@ class VideoServiceTest {
 
         assertTrue(actualMessage.contains(expectedMessage));
     }
+
+    @Test
+    void when_FindByTitle_video_exists_should_return_video_successfully() throws VideoNotFoundException {
+        Video video1 = new Video("code1", "Title1", "Description1", 10.0);
+        Video video2 = new Video("code2", "Title2", "Description2", 20.0);
+        videoService.save(video1);
+        videoService.save(video2);
+
+        List<Video> foundVideos = videoService.findByTitle("Title1");
+        assertEquals(1, foundVideos.size());
+        assertEquals("Title1", foundVideos.get(0).title());
+    }
+
+    @Test
+    void when_FindByTitle_video_does_not_exist_should_throw_VideoNotFoundException() {
+        Exception exception = assertThrows(VideoNotFoundException.class, () -> {
+            videoService.findByTitle("NonExistingTitle");
+        });
+
+        String expectedMessage = "No videos found with title: NonExistingTitle";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void when_FindByDuration_videos_exist_should_return_videos_successfully() throws VideoNotFoundException {
+        Video video1 = new Video("code1", "Title1", "Description1", 10.0);
+        Video video2 = new Video("code2", "Title2", "Description2", 20.0);
+        videoService.save(video1);
+        videoService.save(video2);
+
+        List<Video> foundVideos = videoService.findByDuration(5.0, 15.0);
+        assertEquals(1, foundVideos.size());
+        assertEquals("Title1", foundVideos.get(0).title());
+    }
+
+    @Test
+    void when_FindByDuration_no_videos_found_should_throw_VideoNotFoundException() {
+        Exception exception = assertThrows(VideoNotFoundException.class, () -> {
+            videoService.findByDuration(30.0, 40.0);
+        });
+
+        String expectedMessage = "No videos found in the duration range: 30.0 to 40.0";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }
